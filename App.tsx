e13a26bcdd1916a6d316b2d652a46e53c16c1de0
ovlesser <ovlesser@live.com>
2023-01-27 15:58:30 +1100

    Support another source data type
    - Add the data mapping from another data type to view data in the reducer

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react'
 import MainView from './MainView'
 import { createStore } from 'redux'
-import { setViewDataAction, viewReducer } from './reducer'
+import { addViewDataAction, setViewDataAction, viewReducer } from './reducer'
 import { Provider } from 'react-redux'
 import { useEffect } from 'react'
-import { data1 } from './DataSource'
+import { data1, data2 } from './DataSource'
 
 export const store = createStore(viewReducer)
 
@@ -13,6 +13,9 @@ const App = (): JSX.Element => {
         setTimeout(() => {
             store.dispatch(setViewDataAction(data1))
         }, 1000)
+        setTimeout(() => {
+            store.dispatch(addViewDataAction(data2))
+        }, 5000)
     })
 
     return (
--- reducer.ts
+++ reducer.ts
@@ -1,12 +1,17 @@
-import { Data1 } from './data'
+import { Data1, Data2 } from './data'
 
 interface Action {
     type: string
-    payload: Data1[]
+    payload: Data1[] | Data2[]
 }
 
-export const setViewDataAction = (data: Data1[]): Action => ({
+export const setViewDataAction = (data: Data1[] | Data2[]): Action => ({
     type: 'SET_VIEW_DATA',
+    payload: data
+})
+
+export const addViewDataAction = (data: Data1[] | Data2[]): Action => ({
+    type: 'ADD_VIEW_DATA',
     payload: data
 })
 
@@ -30,6 +35,30 @@ const isData1 = (object: unknown): boolean =>
         object.props instanceof Object &&
         'subType' in object.props
 
+const isData2 = (object: unknown): boolean =>
+    object instanceof Object &&
+    'name' in object &&
+    'props' in object &&
+    object.props instanceof Object &&
+    'nickName' in object.props
+
+const mapToViewData = (data: unknown[]): ViewData[] =>
+    data.reduce((result: ViewData[], element) => {
+        if (isData1(element)) {
+            result.push({
+                title: (element as Data1).type,
+                description: (element as Data1).props.subType
+            })
+        }
+        if (isData2(element)) {
+            result.push({
+                title: (element as Data2).name,
+                description: (element as Data2).props.nickName
+            })
+        }
+        return result
+    }, [])
+
 export const viewReducer = (
     state = defaultState,
     action: Action
@@ -38,16 +67,15 @@ export const viewReducer = (
     case 'SET_VIEW_DATA':
         state = {
             ...state,
-            viewData: action.payload.reduce((result: ViewData[], element) => {
-                if (isData1(element)) {
-                    result.push({
-                        title: (element as Data1).type,
-                        description: (element as Data1).props.subType
-                    })
-                }
-                return result
-            }, [])
+            viewData: mapToViewData(action.payload)
         }
+        break
+    case 'ADD_VIEW_DATA':
+        state = {
+            ...state,
+            viewData: state.viewData.concat(mapToViewData(action.payload))
+        }
+        break
     }
     return state
 }
